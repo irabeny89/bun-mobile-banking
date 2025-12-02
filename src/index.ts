8199d407543a4beb24cc3f99ddf648b48f87302f
Irabeny <irabeny89@gmail.com>
2025-12-02 23:00:43 +0100

    chore: add healthcheck plugin and rewrite log message on start

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import cors from "@elysiajs/cors";
 import { openapi } from "@elysiajs/openapi";
 import { serverTiming } from "@elysiajs/server-timing";
 import { Elysia } from "elysia";
+import { healthcheckPlugin } from "elysia-healthcheck";
+import pkg from "../package.json";
 
 const app = new Elysia()
 	.use(cors())
 	.use(openapi())
 	.use(serverTiming())
+	.use(healthcheckPlugin())
 	.get("/", () => "Hello Elysia")
 	.listen(3000);
 
-console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
+console.log(`🦊 ${pkg.name} is running 🚀`);
+console.log(`Server: ${app.server?.url}`);
+console.log(`API Docs: ${app.server?.url}openapi`);
