8686a2fad310fa155a5ef17f4609cac948e7d317
Irabeny <irabeny89@gmail.com>
2025-12-02 13:55:58 +0100

    chore: add cors, openapi plugins and update readme docs

--- README.md
+++ README.md
@@ -59,9 +59,11 @@ User Type: Business - KYB
 - [ ] Customer service contact options
 
 ## Development
+
 To start the development server run:
 ```bash
 bun run dev
 ```
 
 Open http://localhost:3000/ with your browser to see the result.
+Query `/openapi` endpoint to see OpenAPI documentation.
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import cors from "@elysiajs/cors";
+import { openapi } from "@elysiajs/openapi";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(cors())
+	.use(openapi())
+	.get("/", () => "Hello Elysia")
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
