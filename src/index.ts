6c992dce0dac909db10b428244b0b616c8f55dc3
Irabeny <irabeny89@gmail.com>
2025-12-02 14:41:48 +0100

    chore: install server timing plugin

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import cors from "@elysiajs/cors";
 import { openapi } from "@elysiajs/openapi";
+import { serverTiming } from "@elysiajs/server-timing";
 import { Elysia } from "elysia";
 
 const app = new Elysia()
 	.use(cors())
 	.use(openapi())
+	.use(serverTiming())
 	.get("/", () => "Hello Elysia")
 	.listen(3000);
 
