eb355efdb4caade02202faa6338a96b8d43484b8
Irabeny <irabeny89@gmail.com>
2025-12-03 10:41:12 +0100

    chore: add xss protection and logging

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,40 @@ import { openapi } from "@elysiajs/openapi";
 import { serverTiming } from "@elysiajs/server-timing";
 import { Elysia } from "elysia";
 import { healthcheckPlugin } from "elysia-healthcheck";
+import { elysiaXSS } from "elysia-xss";
+import logixlysia, { type Options } from "logixlysia";
 import pkg from "../package.json";
+import { IS_PROD_ENV } from "./config";
 
+const logOption: Options = {
+	config: {
+		showStartupMessage: false,
+		timestamp: {
+			translateTime: "yyyy-mm-dd HH:MM:ss.SSS",
+		},
+		logFilePath: IS_PROD_ENV ? "./logs/app.log" : undefined,
+		logFilter: IS_PROD_ENV
+			? {
+					level: ["ERROR", "WARNING", "INFO"],
+					status: [500, 404],
+					method: "GET",
+				}
+			: undefined,
+		ip: true,
+		customLogFormat:
+			"🦊 {now} {level} {duration} {method} {pathname} {status} {message} {ip}",
+	},
+};
 const app = new Elysia()
+	.use(logixlysia(logOption))
 	.use(cors())
 	.use(openapi())
 	.use(serverTiming())
 	.use(healthcheckPlugin())
+	.use(elysiaXSS({}))
 	.get("/", () => "Hello Elysia")
 	.listen(3000);
 
-console.log(`🦊 ${pkg.name} is running 🚀`);
+console.log(`🦊 ${pkg.name} v${pkg.version} is running 🚀`);
 console.log(`Server: ${app.server?.url}`);
 console.log(`API Docs: ${app.server?.url}openapi`);
