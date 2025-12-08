8b1de9b4c4a0737801313f565d303b72a3078b98
Irabeny <irabeny89@gmail.com>
2025-12-08 10:03:33 +0100

    feat: Add address and government ID fields to user, make phone optional, and remove photo verification status.

--- src/modules/user/service.ts
+++ src/modules/user/service.ts
@@ -5,11 +5,10 @@ import { UserModel } from "./model";
 export abstract class User { 
   static async findByEmail(sql: Bun.SQL, email: string) {
     const res = await sql`
-      SELECT * FROM users 
+      SELECT id, user_type as "userType", first_name as "firstName", middle_name as "middleName", last_name as "lastName", email, phone, password, photo_id as "photoId", kyc_tier as "kycTier", kyc_status as "kycStatus", mfa_enabled as "mfaEnabled", photo_verified as "photoVerified", business_name as "businessName", business_date as "businessDate", business_type as "businessType", business_industry as "businessIndustry", business_cac as "businessCac", business_tin as "businessTin", kyb_status as "kybStatus", created_at as "createdAt", updated_at as "updatedAt" 
+      FROM users 
       WHERE email = ${email}
-      LIMIT 1
-      RETURNING id, user_type as "userType", first_name as "firstName", middle_name as "middleName", last_name as "lastName", email, phone, password, address
     `
-    return res[0];
+    return res[0] as UserModel.UserT;
   }
 }
