b4e56afc95b31c889a6efdbe112289c4d84d44e6
Irabeny <irabeny89@gmail.com>
2025-12-09 03:04:43 +0100

    refactor: make db a singleton

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,17 +1,22 @@
 import { POSTGRES_URL } from "@/config";
+
+let db: Bun.SQL | null = null;
 /**
  * Initializes/Retrieves the Bun SQL database connection as a singleton.
  * @param url Connection string for the PostgreSQL database. Defaults to POSTGRES_URL from config.
  * @returns Bun SQL instance
  */
-export default function db(url: string = POSTGRES_URL) {
-  return new Bun.SQL({
-    url,
-    onconnect: () => {
-      console.log("Connected to PostgreSQL");
-    },
-    onclose: () => {
-      console.log("PostgreSQL connection closed");
-    },
-  });
+export default function dbSingleton(url: string = POSTGRES_URL) {
+    if (!db) {
+        db = new Bun.SQL({
+            url,
+            onconnect: () => {
+                console.log("Connected to PostgreSQL");
+            },
+            onclose: () => {
+                console.log("PostgreSQL connection closed");
+            },
+        });
+    }
+    return db;
 }
