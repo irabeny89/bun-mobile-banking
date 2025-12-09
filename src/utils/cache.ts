b8cc780df422b59533051024ad598152e6bef6da
Irabeny <irabeny89@gmail.com>
2025-12-09 07:28:26 +0100

    doc: add documentation to function

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -2,6 +2,11 @@ import { VALKEY_URL } from "@/config";
 import { RedisClient } from "bun";
 
 let cache: RedisClient | null = null;
+/**
+ * Cache singleton for Redis/Valkey
+ * @param url optional cache url - redis/valkey
+ * @returns singleton instance
+ */
 export default function cacheSingleton(url: string = VALKEY_URL) {
     if (!cache) {
         cache = new RedisClient(url);
