f6f43365a3cff4cdf4090be47cedc95bd9d8b652
Irabeny <irabeny89@gmail.com>
2025-12-10 03:54:22 +0100

    docs: add documentation to pino-logger utility function

--- src/utils/pino-logger.ts
+++ src/utils/pino-logger.ts
@@ -1,5 +1,10 @@
 import type { Logger } from "logixlysia";
 
+/**
+ * Pino logger utility function to extract the untyped pino logger from the Elysia store object.
+ * @param store Elysia store object
+ * @returns pino logger
+ */
 export default function pinoLogger(store: object) {
     const { pino } = store as { pino: Logger["pino"] };
     return pino
