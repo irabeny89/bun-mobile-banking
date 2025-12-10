30e697e8822d0db83f7838d288a2851144dc3c9c
Irabeny <irabeny89@gmail.com>
2025-12-10 04:10:41 +0100

    feat: Integrate individualUser module, systemStatus, and healthcheck plugins

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import cors from "@elysiajs/cors";
 import { openapi } from "@elysiajs/openapi";
 import { serverTiming } from "@elysiajs/server-timing";
 import { Elysia } from "elysia";
-import { healthcheckPlugin } from "elysia-healthcheck";
 import { elysiaXSS } from "elysia-xss";
 import pkg from "../package.json";
 import { trim } from "./plugins/trim.plugin";
-import { user } from "./modules/user";
 import { errorHandler } from "./plugins/onerror.plugin";
 import { compression } from "./plugins/compress.plugin";
 import { logger } from "./plugins/logger.plugin";
 import { auth } from "./modules/auth";
 import { systemStatus } from "./plugins/system-status.plugin";
+import { individualUser } from "./modules/Individual_user";
+import { healthcheck } from "./plugins/heathcheck.plugin";
 
 const app = new Elysia({
   name: pkg.name,
@@ -26,9 +26,9 @@ const app = new Elysia({
   .use(cors())
   .use(openapi())
   .use(serverTiming())
-  .use(healthcheckPlugin())
   .use(elysiaXSS({}))
   .use(compression)
+  .use(healthcheck)
   .get("/", () => `Hello from ${pkg.name}.\n${pkg.description}`)
-  .group("/api/v1", (app) => app.use(auth).use(user))
+  .group("/api/v1", (app) => app.use(auth).use(individualUser))
   .listen(3000);
