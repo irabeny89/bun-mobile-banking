7a3cf5c6aff4a55aa06e5a041b7b7a393dfe3ae2
Irabeny <irabeny89@gmail.com>
2025-12-12 01:04:41 +0100

    refactor: pass jwt plugin config directly instead of using variable

--- src/plugins/jwt.plugin.ts
+++ src/plugins/jwt.plugin.ts
@@ -1,10 +1,10 @@
 import Elysia from "elysia";
-import jwt, { type JWTOption } from "@elysiajs/jwt";
+import jwt from "@elysiajs/jwt";
 import { SECRET_1 } from "@/config";
 
-const jwtOption: JWTOption<"jwt", undefined> = {
-  name: "jwt",
-  secret: SECRET_1,
-};
-
-export const jwtPlugin = new Elysia({ name: "jwt-plugin" }).use(jwt(jwtOption))
+export const jwtPlugin = new Elysia({ name: "jwt-plugin" })
+    .use(jwt({
+        name: "jwt",
+        secret: SECRET_1,
+    }))
+    .as("scoped")
