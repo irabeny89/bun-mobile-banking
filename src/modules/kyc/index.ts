e8b6ba9509b433fa6aa34ab1a398ea7b68cf9c7c
Irabeny <irabeny89@gmail.com>
2025-12-23 13:18:18 +0100

    feat: rename existing tier 3 verification to address proof.

--- src/modules/kyc/index.ts
+++ src/modules/kyc/index.ts
@@ -4,7 +4,7 @@ import { tier1Verify } from "./routes/tier1-verify";
 import { tier2Status } from "./routes/tier2-status";
 import { tier2Verify } from "./routes/tier2-verify";
 import { tier3Status } from "./routes/tier3-status";
-import { tier3VerifyAddressProof } from "./routes/tier3-verify";
+import { tier3VerifyAddressProof } from "./routes/tier3-verify-address";
 
 export const kyc = new Elysia({
     name: "kyc",
