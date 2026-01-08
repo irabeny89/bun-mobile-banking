8404f13842cc251d0bada8e751407f9ddb3ec989
Irabeny <irabeny89@gmail.com>
2026-01-08 07:19:06 +0100

    perf: improve otp generation logic

--- src/utils/otp.ts
+++ src/utils/otp.ts
@@ -1,13 +1,13 @@
 /**
  * Generates numeric OTP(one-time password) string.
- * @param length number of digits to generate. default is 6
+ * @param digits number of digits to generate. Default is 6, min is 2 & max is 8.
  * @returns numeric OTP string
  */
-export async function genOTP(digits: number = 6): Promise<string> {
-  const { randomInt } = await import("node:crypto");
-  let otp = "";
-  for (let i = 0; i < digits; i++) {
-    otp += randomInt(10);
-  }
-  return otp;
+export async function genOTP(digits: 2 | 3 | 4 | 5 | 6 | 7 | 8 = 6): Promise<string> {
+    if (digits < 2 || digits > 8) {
+        throw new Error("Invalid number of digits");
+    }
+    return Date.now()
+        .toString(10)
+        .slice(-digits);
 }
