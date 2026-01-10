2116ce5daf7f7b3884305ee3ca4285ad5e92d858
Irabeny <irabeny89@gmail.com>
2026-01-10 19:14:03 +0100

    chore: remove useless rows from kyc table

--- src/migrations/1767565323069-create-individual-user-account-table.sql
+++ src/migrations/1767565323069-create-individual-user-account-table.sql
@@ -1,21 +1,11 @@
 CREATE TABLE IF NOT EXISTS individual_accounts (
     id UUID PRIMARY KEY DEFAULT uuidv7(),
     user_id UUID REFERENCES individual_users(id) ON DELETE CASCADE,
-    account_number VARCHAR(20),
-    account_name VARCHAR(100),
-    account_type VARCHAR(30),
-    currency VARCHAR(10),
-    balance BIGINT NOT NULL DEFAULT 0,
-    institution_id VARCHAR(30),
-    institution_auth_method VARCHAR(25),
-    institution_name VARCHAR(100),
-    institution_bank_code VARCHAR(20),
-    institution_type VARCHAR(30),
     mono_account_id VARCHAR(30) NOT NULL,
     mono_customer_id VARCHAR(30) NOT NULL,
     mono_reference VARCHAR(10) NOT NULL,
-    mono_reauthorize BOOLEAN NOT NULL DEFAULT FALSE,
     mono_data_status VARCHAR(15),
+    mono_reauthorize BOOLEAN NOT NULL DEFAULT FALSE,
     mfa BOOLEAN NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
@@ -36,3 +26,5 @@ EXECUTE FUNCTION set_update_timestamp();
 
 CREATE INDEX IF NOT EXISTS idx_account_created_at ON individual_accounts(created_at);
 CREATE INDEX IF NOT EXISTS idx_account_mono_account_id ON individual_accounts(mono_account_id);
+CREATE INDEX IF NOT EXISTS idx_account_user_id ON individual_accounts(user_id);
+
