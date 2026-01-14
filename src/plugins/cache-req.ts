55524008c6709026250906cdcc39b3a540b8c358
Irabeny <irabeny89@gmail.com>
2026-01-14 11:51:09 +0100

    fix: replace onAfterHandle with onAfterResponse in cacheReq plugin

--- src/plugins/cache-req.ts
+++ src/plugins/cache-req.ts
@@ -4,25 +4,25 @@ import { CACHE_GET } from "../config";
 import { CACHE_GET_HEADER_VALUE } from "@/types";
 import pinoLogger from "../utils/pino-logger";
 
+const cache = cacheSingleton()
 export const cacheReq = new Elysia({ name: "cache-request" })
     .state("cacheHit", false)
-    .decorate("cache", cacheSingleton())
     .resolve(({ store }) => ({ logger: pinoLogger(store) }))
-    .onAfterHandle(async ({ store, cache, request, set, responseValue, logger }) => {
+    .onAfterResponse(async ({ store, request, response, set, logger }) => {
         // cache if GET request has response and cache hit was missed
-        if (request.method === "GET" && !!responseValue && !store.cacheHit) {
+        if (request.method === "GET" && !!response && !store.cacheHit) {
             logger.info("cacheReq:: caching response")
-            await cache.set(request.url, JSON.stringify(responseValue))
+            await cache.set(request.url, JSON.stringify(response))
             await cache.expire(request.url, CACHE_GET.ttl)
             set.headers[CACHE_GET.header] = CACHE_GET_HEADER_VALUE.Set
             set.headers[CACHE_GET.ttlHeader] = CACHE_GET.ttl
-            logger.info({ url: request.url, responseValue }, "cacheReq:: response cached")
+            logger.info({ url: request.url, response }, "cacheReq:: response cached")
         }
     })
-    .onBeforeHandle(async ({ request, store, set, cache, logger }) => {
+    .onBeforeHandle(async ({ request, store, set, logger }) => {
         logger.info("cacheReq:: checking cache")
-        const cached = await cache.get(request.url);
-        if (cached) {
+        const cached = request.method === "GET" && await cache.get(request.url);
+        if (!!cached) {
             logger.info("cacheReq:: cache hit")
             const responseValue = JSON.parse(cached)
             store.cacheHit = true;
