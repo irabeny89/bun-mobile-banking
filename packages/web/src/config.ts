7cc1ad148ddadc8115759dbb472974e08233c4d3
Irabeny <irabeny89@gmail.com>
2026-01-21 05:54:24 +0100

    feat: Allow API URL to be configured via environment variables and ignore .env files.

--- packages/web/src/config.ts
+++ packages/web/src/config.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = "MoBa";
-export const API_URL = "http://localhost:3001";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 export const ACCESS_TOKEN_KEY = "accessToken";
